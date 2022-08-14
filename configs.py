970c1dfff07417db8abda63ba4bb08b5c8153f4d
fpband <82159763+fpband@users.noreply.github.com>
2022-08-14 22:19:43 +0430

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
-# (c) @AbirHasan2005
+# (c) @FarshidBand
 
 import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
+	API_ID = int(os.environ.get("API_ID"))
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
@@ -19,34 +19,20 @@ class Config(object):
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
-This is Permanent Files Store Bot!
-Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
+🤖 **نام ربات :** [ذخیره ساز انواع فایل](https://t.me/{BOT_USERNAME})
 
-🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
+🧑🏻‍💻 **سازنده ربات :** @FarshidBand
 
-📝 **Language:** [Python3](https://www.python.org)
-
-📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
-
-📡 **Hosted on:** [Heroku](https://heroku.com)
-
-🧑🏻‍💻 **Developer:** @AbirHasan2005
-
-👥 **Support Group:** [Linux Repositories](https://t.me/DevsZone)
-
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
+📢 **کانال پشتیبانی :** [Channel](https://t.me/irbotz)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
-
-Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
-
-Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
-
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
+ابتدا در کانال خواسته شده عضو شوید‌.
+سپس روی لینک درج شده داخل کانال 
+کلیک کنید.
+سپس به ربات هدایت میشوید روی start کلیک کنید فایل مورد نظر براتون ارسال میشه.
 """
 	HOME_TEXT = """
-Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
-
-Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
+**سلام [{}](tg://user?id={}) عزیز خوش آمدید ❤**\n\n**• من ربات ذخیره ساز انواع فایل هستم**.
+**🎭 جهت دریافت فایل روی لینک های داخل کانال سریزپلاس کلیک کنید.
 """
+Footer
